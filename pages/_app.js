6fc25e6f15be09e2822cde0ec0bee240d2937cf1
Fayaze H. Pantho <60003361+Fayazexo@users.noreply.github.com>
2021-08-14 04:37:26 +0600

    Added Scroll Bar Design

--- pages/_app.js
+++ pages/_app.js
@@ -17,10 +17,25 @@ const GlobalStyle = ({ children }) => (
           color: #fefefe;
         }
 
-        html {
+          html {
           min-width: 360px;
           scroll-behavior: smooth;
         }
+
+        body::-webkit-scrollbar {
+        width: 1em;
+      }
+      a{
+        cursor: url("custom.gif"), url("custom.cur"), default;
+      }
+        body::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+        background: linear-gradient(rgba(10,245,244,1) 0%, rgba(9,219,31,1) 50%, rgba(247,243,34,1) 100%);
+      }
+
+        body::-webkit-scrollbar-thumb {
+        background-color: #e8e8e8;
+      }
 
         #__next {
           display: flex;
